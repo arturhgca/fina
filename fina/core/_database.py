2e325e4770120bd14c96e897cd9a081e7ff5d1d4
arturhgca <arturhgca@gmail.com>
2022-02-27 14:44:52 -0300

    Add common attributes to base model class

--- fina/core/_database.py
+++ fina/core/_database.py
@@ -1,8 +1,28 @@
-from peewee import SqliteDatabase, Model
+from __future__ import annotations
+
+import datetime
+
+from peewee import SqliteDatabase, Model, DateTimeField, BooleanField
 
 db = SqliteDatabase("fina-core.db")
 
 
 class BaseModel(Model):
+    created_on = DateTimeField(default=datetime.datetime.now)
+    updated_on = DateTimeField(default=datetime.datetime.now)
+    is_deleted = BooleanField(default=False)
+
+    def soft_delete(self) -> None:
+        self.is_deleted = True
+        self.save()
+
+    def undelete(self) -> None:
+        self.is_deleted = False
+        self.save()
+
+    @classmethod
+    def new(cls, **kwargs) -> BaseModel:
+        raise NotImplementedError
+
     class Meta:
         database = db
